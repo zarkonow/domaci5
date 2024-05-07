8aaf2e5cbddeba8ccdcd20571aa25fb1644bcf30
Zarko Stojanov <stojanov1987@gmail.com>
2024-05-07 22:07:31 +0200

    * ini commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'a76b1b19'
+//http://www.omdbapi.com/?t=Terminator&apikey=a76b1b19
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 
