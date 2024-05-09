b5ef3cd72d3b9b9bdf762944d3d22a00e1c162c0
Zarko Stojanov <stojanov1987@gmail.com>
2024-05-09 19:24:31 +0200

    * aded input, btn, state * axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import axios from "axios";
 import Movies from "./Components/Movies";
 
-axios.get(process.env.REACT_APP_OMDBAPI_URL+'?t=Terminator&apikey='+process.env.REACT_APP_OMDBAPI_KEY)
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
 
 const App = () => {
   return (
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,9 +1,24 @@
-const Movies = () => {
-    return(
-        <>
+import {useState} from "react";
+import axios from "axios";
 
+const Movies = () => {
+
+    const [movieName, setMovieName] = useState()
+
+    const searchMovieByTitle = () =>{
+        axios.get(`${process.env.REACT_APP_OMDBAPI_URL}?t=${movieName}&apikey=${process.env.REACT_APP_OMDBAPI_KEY}`)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error))
+
+    }
+
+    return <>
+        <form>
+            <input type='text' onInput={e => setMovieName(e.currentTarget.value)} />
+            <button onClick={searchMovieByTitle} type='button'>Search</button>
+        </form>
         </>
-    )
+
 }
 
 export default Movies
